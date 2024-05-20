fea6e4c65b813aac1436803e64980cfad6d3566d
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-20 13:43:37 -0300

    Projeto inicial

---
+++ assets/ApiClinicSystem/src/main/java/br/com/sistema/clinica/ApiClinicSystem/ApiClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistema.clinica.ApiClinicSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicSystemApplication.class, args);
+	}
+
+}
---
+++ assets/ApiClinicSystem/src/test/java/br/com/sistema/clinica/ApiClinicSystem/ApiClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema.clinica.ApiClinicSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
