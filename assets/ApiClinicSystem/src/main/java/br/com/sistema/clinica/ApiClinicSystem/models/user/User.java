d25528e9186cba6efd671976995392c2e91c854f
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-28 15:28:18 -0300

    Entidade Usuário

---
+++ assets/ApiClinicSystem/src/main/java/br/com/sistema/clinica/ApiClinicSystem/models/user/User.java
@@ -0,0 +1,23 @@
+package br.com.sistema.clinica.ApiClinicSystem.models.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String password;
+}
