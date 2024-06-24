8ce01f13f696383c85a7e37202f05a2d539948e0
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-06-24 14:22:07 -0300

    TODO
    Estas classes estão com erro

--- assets/ApiClinicSystem/src/main/java/br/com/sistema/clinica/ApiClinicSystem/validation/ValidateIfDoctorThereIsAnAppointment.java
+++ assets/ApiClinicSystem/src/main/java/br/com/sistema/clinica/ApiClinicSystem/validation/ValidateIfDoctorThereIsAnAppointment.java
@@ -1,25 +1,26 @@
-package br.com.sistema.clinica.ApiClinicSystem.validation;
-
-import br.com.sistema.clinica.ApiClinicSystem.dto.consultationDto.ScheduleAppointmentDTO;
-import br.com.sistema.clinica.ApiClinicSystem.infra.error.ExceptionValidation;
-import br.com.sistema.clinica.ApiClinicSystem.repository.consultation.IConsultationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-// VALIDAR SE EXISTE UMA MARCAÇÃO NO MESMO HORÁRIO
-
-@Component
-public class ValidateIfDoctorThereIsAnAppointment implements AppointmentValidator{
-
-    @Autowired
-    private IConsultationRepository iConsultationRepository;
-
-    public void validate(ScheduleAppointmentDTO scheduleAppointmentDTO){
-
-        var doctorHasAnotherAppointment =  iConsultationRepository.existsByIdMedico(scheduleAppointmentDTO.idDoctor(), scheduleAppointmentDTO.data());
-
-        if (doctorHasAnotherAppointment) {
-            throw new ExceptionValidation("Médico já possui outra consulta neste mesmo horário.");
-        }
-    }
-}
+//package br.com.sistema.clinica.ApiClinicSystem.validation;
+//
+//import br.com.sistema.clinica.ApiClinicSystem.dto.consultationDto.ScheduleAppointmentDTO;
+//import br.com.sistema.clinica.ApiClinicSystem.infra.error.ExceptionValidation;
+//import br.com.sistema.clinica.ApiClinicSystem.repository.consultation.IConsultationRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//// VALIDAR SE EXISTE UMA MARCAÇÃO NO MESMO HORÁRIO
+//
+//@Component
+//public class ValidateIfDoctorThereIsAnAppointment implements AppointmentValidator{
+//
+//    @Autowired
+//    private IConsultationRepository iConsultationRepository;
+//
+//    @Override
+//    public void validate(ScheduleAppointmentDTO scheduleAppointmentDTO){
+//
+//        var doctorHasAnotherAppointment =  iConsultationRepository.existsByIdMedico(scheduleAppointmentDTO.idDoctor(), scheduleAppointmentDTO.data());
+//
+//        if (doctorHasAnotherAppointment) {
+//            throw new ExceptionValidation("Médico já possui outra consulta neste mesmo horário.");
+//        }
+//    }
+//}
--- assets/ApiClinicSystem/src/main/java/br/com/sistema/clinica/ApiClinicSystem/validation/ValidateIfPacienteThereIsAppointment.java
+++ assets/ApiClinicSystem/src/main/java/br/com/sistema/clinica/ApiClinicSystem/validation/ValidateIfPacienteThereIsAppointment.java
@@ -1,26 +1,26 @@
-package br.com.sistema.clinica.ApiClinicSystem.validation;
-
-import br.com.sistema.clinica.ApiClinicSystem.dto.consultationDto.ScheduleAppointmentDTO;
-import br.com.sistema.clinica.ApiClinicSystem.infra.error.ExceptionValidation;
-import br.com.sistema.clinica.ApiClinicSystem.repository.consultation.IConsultationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-// VERIFICAR SE O PACIENTE JÁ POSSUI UMA CONSULTA NO DIA
-
-@Component
-public class ValidateIfPacienteThereIsAppointment implements AppointmentValidator{
-
-    @Autowired
-    private IConsultationRepository iConsultationRepository;
-
-    public void validate(ScheduleAppointmentDTO scheduleAppointmentDTO) {
-        var firstHour = scheduleAppointmentDTO.data().withHour(7);
-        var lasthour  = scheduleAppointmentDTO.data().withHour(18);
-        var pacienteHasAnotherAppointment = iConsultationRepository.existsByPacienteAndDataBetween(scheduleAppointmentDTO.idPaciente(), firstHour, lasthour);
-
-        if (pacienteHasAnotherAppointment) {
-            throw new ExceptionValidation("Paciente já possui uma consulta neste mesmo horário.");
-        }
-    }
-}
+//package br.com.sistema.clinica.ApiClinicSystem.validation;
+//
+//import br.com.sistema.clinica.ApiClinicSystem.dto.consultationDto.ScheduleAppointmentDTO;
+//import br.com.sistema.clinica.ApiClinicSystem.infra.error.ExceptionValidation;
+//import br.com.sistema.clinica.ApiClinicSystem.repository.consultation.IConsultationRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//// VERIFICAR SE O PACIENTE JÁ POSSUI UMA CONSULTA NO DIA
+//
+//@Component
+//public class ValidateIfPacienteThereIsAppointment implements AppointmentValidator{
+//
+//    @Autowired
+//    private IConsultationRepository iConsultationRepository;
+//
+//    public void validate(ScheduleAppointmentDTO scheduleAppointmentDTO) {
+//        var firstHour = scheduleAppointmentDTO.data().withHour(7);
+//        var lasthour  = scheduleAppointmentDTO.data().withHour(18);
+//        var pacienteHasAnotherAppointment = iConsultationRepository.existsByPacienteAndDataBetween(scheduleAppointmentDTO.idPaciente(), firstHour, lasthour);
+//
+//        if (pacienteHasAnotherAppointment) {
+//            throw new ExceptionValidation("Paciente já possui uma consulta neste mesmo horário.");
+//        }
+//    }
+//}
