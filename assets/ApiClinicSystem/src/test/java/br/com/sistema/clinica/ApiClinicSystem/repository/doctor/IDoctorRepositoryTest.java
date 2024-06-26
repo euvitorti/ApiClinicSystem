a992aa9128bb4d314f0f697482ef778a568e5b3c
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-06-26 15:06:20 -0300

    Cenário de testes

--- assets/ApiClinicSystem/src/test/java/br/com/sistema/clinica/ApiClinicSystem/repository/doctor/IDoctorRepositoryTest.java
+++ assets/ApiClinicSystem/src/test/java/br/com/sistema/clinica/ApiClinicSystem/repository/doctor/IDoctorRepositoryTest.java
@@ -1,11 +1,18 @@
 package br.com.sistema.clinica.ApiClinicSystem.repository.doctor;
 
+import br.com.sistema.clinica.ApiClinicSystem.models.doctor.SpecialtyEnum;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 // A ANOTAÇÃO @DataJpaTest, é utilizada para testar uma interface Repository
 // A ANOTAÇÃO AutoConfigureTestDatabase.Replace.NONE, INFORMA AO SPRING,
@@ -16,10 +23,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 class IDoctorRepositoryTest {
 
+    @Autowired
+    private IDoctorRepository iDoctorRepository;
+
+//    @Autowired
+//    private TestEntityManager testEntityManager;
+
     @Test
-    void chooseFreeDoctorOnTheDate() {
+    @DisplayName("SHOULD RETURN NULL WHEN THE ONLY REGISTERED DOCTOR IS NOT AVAILABLE ON THE DATE")
+    void chooseFreeDoctorOnTheDateScenario1() {
+
+        var nextMondayAt10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+
+        // O BANCO DEVE TER ALGUNS DADOS JÁ CADASTRADOS PARA O TESTE
+        // ESTAREI USANDO O BANCO JÁ CRIADO, PORÉM POR MOTIVOS DE SEGURANÇA
+        // CRIE UM SEPARADO COM OS DADOS, PARA FINS DE TESTE
+
+        var doctorFree = iDoctorRepository.ChooseFreeDoctorOnTheDate(SpecialtyEnum.CARDIOLOGIA, nextMondayAt10);
+
+        assertThat(doctorFree).isNull();
     }
+
+    @Test
+    @DisplayName("SHOULD RETURN DOCTOR WHEN THE DOCTOR IS AVAILABLE ON THE DATE")
+    void chooseFreeDoctorOnTheDateScenario2() {
+
+        var nextMondayAt10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10,0);
+
+        var doctorFree = iDoctorRepository.ChooseFreeDoctorOnTheDate(SpecialtyEnum.CARDIOLOGIA, nextMondayAt10);
+
+        assertThat(doctorFree).isEqualTo(doctorFree);
+    }
+
 }
