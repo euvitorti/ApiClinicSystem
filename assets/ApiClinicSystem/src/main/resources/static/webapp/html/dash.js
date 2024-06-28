01ddaa050044777aae9c8b91d58d12b1fbc61f04
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-06-28 15:52:33 -0300

    Refatoração

---
+++ assets/ApiClinicSystem/src/main/resources/static/webapp/html/dash.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#register-patient').addEventListener('click', function () {
+        window.location.href = 'registerPaciente.html';
+    });
+});
