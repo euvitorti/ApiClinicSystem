4afbafb3c1ce060a9e2afa0efa5298a1fef858f8
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-07-05 15:46:06 -0300

    Atualizar pacientes

--- assets/ApiClinicSystem/src/main/resources/static/webapp/html/menu.js
+++ assets/ApiClinicSystem/src/main/resources/static/webapp/html/menu.js
@@ -5,3 +5,7 @@ document.getElementById('register-patient').addEventListener('click', function()
 document.getElementById('list-patients').addEventListener('click', function() {
     window.location.href = 'listPacientes.html';
 });
+
+document.getElementById('update-patients').addEventListener('click', function() {
+    window.location.href = 'updatePaciente.html';
+});
